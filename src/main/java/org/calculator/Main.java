fac2463b0270e537047cb32d22257115f20a9af9
VemulaNitheesh <vemulanitheesh05@gmail.com>
2026-03-04 15:27:47 +0530

    Added Subtract and checking git trigger

--- src/main/java/org/calculator/Main.java
+++ src/main/java/org/calculator/Main.java
@@ -6,12 +6,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         Calculator calc = new Calculator();
 
-        System.out.println("Addition Calculator");
+        System.out.println("Calculator");
+
+        System.out.println("Choose operation:");
+        System.out.println("1 → Addition");
+        System.out.println("2 → Subtraction");
+
+        int choice = sc.nextInt();
 
         System.out.println("Enter first number:");
         double a = sc.nextDouble();
@@ -19,7 +24,19 @@ public class Main {
         System.out.println("Enter second number:");
         double b = sc.nextDouble();
 
-        double result = calc.add(a,b);
+        double result;
+
+        if(choice == 1) {
+            result = calc.add(a, b);
+        }
+        else if(choice == 2) {
+            result = calc.subtract(a, b);
+        }
+        else {
+            System.out.println("Invalid choice");
+            sc.close();
+            return;
+        }
 
         System.out.println("Result = " + result);
 
