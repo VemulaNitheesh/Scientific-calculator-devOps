fb0b5e32ef5189b73ab8764ad5e4bdc02d8668a8
VemulaNitheesh <vemulanitheesh05@gmail.com>
2026-03-05 11:55:51 +0530

    bugs fixed

--- src/main/java/org/calculator/Main.java
+++ src/main/java/org/calculator/Main.java
@@ -13,28 +13,35 @@ public class Main {
 
         while (true) {
 
+            int choice = -1;
+
             System.out.println("Choose operation:");
             System.out.println("1 → Addition");
             System.out.println("2 → Subtraction");
             System.out.println("3 → Exit");
 
-            if (!sc.hasNextInt()) {
-                System.out.println("Invalid input.");
+            if (sc.hasNextInt()) {
+                choice = sc.nextInt();
+            } else {
+                System.out.println("Invalid input. Try again.");
                 sc.next();
                 continue;
             }
-
-            int choice = sc.nextInt();
 
             if (choice == 3) {
                 System.out.println("Exiting calculator...");
                 break;
             }
 
+            if (choice != 1 && choice != 2) {
+                System.out.println("Invalid choice. Try again.");
+                continue;
+            }
+
             System.out.println("Enter first number:");
 
             if (!sc.hasNextDouble()) {
-                System.out.println("Invalid first number.");
+                System.out.println("Invalid number.");
                 sc.next();
                 continue;
             }
@@ -44,7 +51,7 @@ public class Main {
             System.out.println("Enter second number:");
 
             if (!sc.hasNextDouble()) {
-                System.out.println("Invalid second number.");
+                System.out.println("Invalid number.");
                 sc.next();
                 continue;
             }
@@ -55,11 +62,8 @@ public class Main {
 
             if (choice == 1) {
                 result = calc.add(a, b);
-            } else if (choice == 2) {
-                result = calc.subtract(a, b);
             } else {
-                System.out.println("Invalid choice");
-                continue;
+                result = calc.subtract(a, b);
             }
 
             System.out.println("Result = " + result);
